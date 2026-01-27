0656f362cb9ece1ac995a2c3380ebff2aaf948fd
hayato <tnkhyt2089@gmail.com>
2026-01-27 20:58:09 +0900

    import ecs_service_auto_scaling_target,policy

--- modules/aws/ecs/main.tf
+++ modules/aws/ecs/main.tf
@@ -47,17 +47,63 @@ resource "aws_ecs_service" "slack_metrics_api" {
   dynamic "load_balancer" {
     for_each = var.slack_metrics_api.target_group_arn != null ? [1] : []
     content {
-    container_name   = "api"
-    container_port   = 8080
-    elb_name         = null
-    target_group_arn = var.slack_metrics_api.target_group_arn
-  }
+      container_name   = "api"
+      container_port   = 8080
+      elb_name         = null
+      target_group_arn = var.slack_metrics_api.target_group_arn
+    }
   }
   network_configuration {
     assign_public_ip = false
     security_groups  = var.slack_metrics_api.security_group_ids
     subnets          = var.slack_metrics_api.subnet_ids
   }
+}
+
+
+resource "aws_appautoscaling_target" "slack_metrics_api" {
+  service_namespace  = "ecs"
+  max_capacity       = 2
+  min_capacity       = 1
+  resource_id        = "service/${aws_ecs_cluster.cloud_pratica_backend.name}/${var.slack_metrics_api.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  depends_on         = [aws_ecs_service.slack_metrics_api]
+}
+
+resource "aws_appautoscaling_policy" "slack_metrics_api_cpu" {
+  name               = "target-tracking-cpu"
+  policy_type        = "TargetTrackingScaling"
+  region             = "ap-northeast-1"
+  resource_id        = aws_appautoscaling_target.slack_metrics_api.resource_id
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+  target_tracking_scaling_policy_configuration {
+    disable_scale_in   = false
+    scale_in_cooldown  = 300
+    scale_out_cooldown = 60
+    target_value       = 70
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
   }
+}
+
+resource "aws_appautoscaling_policy" "slack_metrics_api_memory" {
+  name               = "target-tracking-memory"
+  policy_type        = "TargetTrackingScaling"
+  region             = "ap-northeast-1"
+  resource_id        = aws_appautoscaling_target.slack_metrics_api.resource_id
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+  target_tracking_scaling_policy_configuration {
+    disable_scale_in   = false
+    scale_in_cooldown  = 300
+    scale_out_cooldown = 60
+    target_value       = 70
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+    }
+  }
+}
 
 
