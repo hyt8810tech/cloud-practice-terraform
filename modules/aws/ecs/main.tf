293f3dd6a40a79db940bbe1bff7528570a481d32
hayato <tnkhyt2089@gmail.com>
2026-02-11 20:56:13 +0900

    add ecspresso prd

--- modules/aws/ecs/main.tf
+++ modules/aws/ecs/main.tf
@@ -22,43 +22,43 @@ resource "aws_ecs_cluster_capacity_providers" "cloud_pratica_backend" {
 /**********************************************************
 ecs service slack-metrics-api
 **********************************************************/
-resource "aws_ecs_service" "slack_metrics_api" {
-  cluster                            = aws_ecs_cluster.cloud_pratica_backend.arn
-  name                               = var.slack_metrics_api.name
-  deployment_maximum_percent         = 200
-  deployment_minimum_healthy_percent = 100
-  desired_count                      = 0
-  enable_ecs_managed_tags            = true
-  enable_execute_command             = true
-  health_check_grace_period_seconds  = 0
-  task_definition                    = var.slack_metrics_api.task_definition
-  capacity_provider_strategy {
-    base              = 0
-    capacity_provider = var.slack_metrics_api.capacity_provider
-    weight            = 1
-  }
-  deployment_circuit_breaker {
-    enable   = true
-    rollback = true
-  }
-  deployment_controller {
-    type = "ECS"
-  }
-  dynamic "load_balancer" {
-    for_each = var.slack_metrics_api.target_group_arn != null ? [1] : []
-    content {
-      container_name   = "api"
-      container_port   = 8080
-      elb_name         = null
-      target_group_arn = var.slack_metrics_api.target_group_arn
-    }
-  }
-  network_configuration {
-    assign_public_ip = false
-    security_groups  = var.slack_metrics_api.security_group_ids
-    subnets          = var.slack_metrics_api.subnet_ids
-  }
-}
+# resource "aws_ecs_service" "slack_metrics_api" {
+#   cluster                            = aws_ecs_cluster.cloud_pratica_backend.arn
+#   name                               = var.slack_metrics_api.name
+#   deployment_maximum_percent         = 200
+#   deployment_minimum_healthy_percent = 100
+#   desired_count                      = 0
+#   enable_ecs_managed_tags            = true
+#   enable_execute_command             = true
+#   health_check_grace_period_seconds  = 0
+#   task_definition                    = var.slack_metrics_api.task_definition
+#   capacity_provider_strategy {
+#     base              = 0
+#     capacity_provider = var.slack_metrics_api.capacity_provider
+#     weight            = 1
+#   }
+#   deployment_circuit_breaker {
+#     enable   = true
+#     rollback = true
+#   }
+#   deployment_controller {
+#     type = "ECS"
+#   }
+#   dynamic "load_balancer" {
+#     for_each = var.slack_metrics_api.target_group_arn != null ? [1] : []
+#     content {
+#       container_name   = "api"
+#       container_port   = 8080
+#       elb_name         = null
+#       target_group_arn = var.slack_metrics_api.target_group_arn
+#     }
+#   }
+#   network_configuration {
+#     assign_public_ip = false
+#     security_groups  = var.slack_metrics_api.security_group_ids
+#     subnets          = var.slack_metrics_api.subnet_ids
+#   }
+# }
 
 
 resource "aws_appautoscaling_target" "slack_metrics_api" {
@@ -67,7 +67,7 @@ resource "aws_appautoscaling_target" "slack_metrics_api" {
   min_capacity       = 1
   resource_id        = "service/${aws_ecs_cluster.cloud_pratica_backend.name}/${var.slack_metrics_api.name}"
   scalable_dimension = "ecs:service:DesiredCount"
-  depends_on         = [aws_ecs_service.slack_metrics_api]
+  # depends_on         = [aws_ecs_service.slack_metrics_api]
 }
 
 resource "aws_appautoscaling_policy" "slack_metrics_api_cpu" {
--- prd/variables.tf
+++ prd/variables.tf
@@ -1,10 +1,10 @@
 locals {
-  env        = "prd"
-  account_id = "165115313503"
-  region     = "ap-northeast-1"
-  base_host  = "8810-infra.click"
-  slack_metrics_host = "sm.${local.base_host}"
-  slack_metrics_api_host = "sm-api.${local.base_host}"
+  env                               = "prd"
+  account_id                        = "948743119225"
+  region                            = "ap-northeast-1"
+  base_host                         = "8810-infra.click"
+  slack_metrics_host                = "sm.${local.base_host}"
+  slack_metrics_api_host            = "sm-api.${local.base_host}"
   amplify_domain_name_slack_metrics = "main.d3frdxuhjsb93b.amplifyapp.com"
   public_subnet_ids = [
     module.subnet.id_public_subnet_1a,
